7348be6c9d35e673ae4b26c8f13ed9f9849e7ed0
emonsimply <emonsimply@gmail.com>
2025-08-26 14:37:51 +0600

    NavBar added

---
+++ src/pages/Home/AllPackages/AllPackages.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllPackages = () => {
+  return (
+    <div>
+      <h1>AllPackages</h1>
+    </div>
+  );
+};
+
+export default AllPackages;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home";
 
-const Router = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
+  },
+]);
 
 export default Router;
